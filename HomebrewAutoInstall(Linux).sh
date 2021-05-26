dc231f9ebe314fa4ca5332cebc3929d91278de36
Mintimate <mintimate215@gmail.com>
2021-05-26 15:58:34 +0800

    Update HomebrewAutoInstall(Linux).sh

--- HomebrewAutoInstall(Linux).sh
+++ HomebrewAutoInstall(Linux).sh
@@ -6,8 +6,7 @@ elif [ `uname -m` != 'x86_64' ];then
     exit 0
 elif [ `uname` = "Darwin" ];then
     echo '\033[1;31m本Homebrew安装脚本为Linux版本，macOS用户请查看：
-        https://www.mintimate.cn/2020/04/05/Homebrew
-     \033[0m'
+        https://www.mintimate.cn/2020/04/05/Homebrew \033[0m'
     exit 0
 fi
 
